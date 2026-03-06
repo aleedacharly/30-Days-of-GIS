e9ca64b4b13b45980116cc7e15e49cb2d831c9be
Aleeda Charly <aleeda@gfz.de>
2026-03-06 10:07:28 +0100

    Analyisis code

---
+++ Day3_Heatmap_Berlin/scripts/2_lst_analysis.py
@@ -0,0 +1,48 @@
+import geopandas as gpd
+import rasterio
+from rasterstats import zonal_stats
+import numpy as np
+import pandas as pd
+
+
+# Load Berlin district boundaries
+districts = gpd.read_file('data/raw/berlin_districts.geojson')
+
+
+# Reproject to match raster CRS (EPSG:25833)
+districts = districts.to_crs('EPSG:25833')
+
+
+# Compute zonal statistics for LST
+lst_stats = zonal_stats(
+    districts,
+    'data/raw/lst_berlin.tif',
+    stats=['mean', 'min', 'max', 'std'],
+    nodata=-9999
+)
+
+
+# Do the same for NDVI
+ndvi_stats = zonal_stats(
+    districts,
+    'data/raw/ndvi_berlin.tif',
+    stats=['mean'],
+    nodata=-9999
+)
+
+
+# Add stats to districts GeoDataFrame
+districts['lst_mean'] = [s['mean'] for s in lst_stats]
+districts['lst_min']  = [s['min']  for s in lst_stats]
+districts['lst_max']  = [s['max']  for s in lst_stats]
+districts['lst_std']  = [s['std']  for s in lst_stats]
+districts['ndvi_mean'] = [s['mean'] for s in ndvi_stats]
+
+
+# Save processed GeoJSON and CSV
+districts.to_file('data/processed/berlin_districts_lst.geojson', driver='GeoJSON')
+districts[['name','lst_mean','lst_min','lst_max','ndvi_mean']].to_csv(
+    'outputs/uhi_district_stats.csv', index=False)
+
+
+print(districts[['name','lst_mean','ndvi_mean']].sort_values('lst_mean', ascending=False))
--- Day3_Heatmap_Berlin/scripts/get_berlin_ops_boundary.py
+++ Day3_Heatmap_Berlin/scripts/get_berlin_ops_boundary.py
@@ -1,12 +1,4 @@
 import osmnx as ox
-import geopandas as gpd
-
-# Download Berlin political boundary
 berlin = ox.geocode_to_gdf('Berlin, Germany')
-
-# Reproject to WGS84 (required for GEE upload)
-berlin = berlin.to_crs('EPSG:4326')
-
-# Save as shapefile
-berlin.to_file('data/raw/berlin_boundary.shp')
-print('Saved shapefile files in data/raw/')
+berlin.to_file('data/raw/berlin_boundary.geojson', driver='GeoJSON')
+print(berlin.crs)
